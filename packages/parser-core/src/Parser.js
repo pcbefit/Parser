7ff76e8b9a3022a27f4d7579661a48d191234d70
Tyreal Gray <tyrealgray@ymail.com>
2020-05-28 21:50:43 +0100

    define setter API

--- packages/parser-core/src/Parser.js
+++ packages/parser-core/src/Parser.js
@@ -1,15 +1,66 @@
 class Parser {
-	constructor(){
-
+	constructor() {
+		this._currentSetup = {
+			motherboard: null,
+			CPU: null,
+			GPU: [],
+			ram: [],
+			SSD: [],
+			hardDrive: [],
+			ROM: [],
+		};
 	}
 
-	load(component){
-
+	load(component) {
+		this._currentSetup = component;
 	}
 
-	currentSetup(){
-		return {};
+	getAutoSetup() {
+		return this._currentSetup;
+	}
+
+	checkSetup(){
+		return false;
+	}
+
+	setCPU(CPU){
+		const oldSetup = this._currentSetup;
+		this._currentSetup = { ...oldSetup, CPU };
+	}
+
+	setGPU(GPU){
+		const oldSetup = this._currentSetup;
+		this._currentSetup = { ...oldSetup, GPU };
+	}
+
+	setHardDrive(hardDrive){
+		const oldSetup = this._currentSetup;
+		this._currentSetup = { ...oldSetup, hardDrive };
+	}
+
+	setSSD(SSD){
+		const oldSetup = this._currentSetup;
+		this._currentSetup = { ...oldSetup, SSD };
+	}
+
+	setROM(ROM){
+		const oldSetup = this._currentSetup;
+		this._currentSetup = { ...oldSetup, ROM };
+	}
+
+	setMotherboard(motherboard){
+		const oldSetup = this._currentSetup;
+		this._currentSetup = { ...oldSetup, motherboard };
+	}
+
+	setRam(ram){
+		const oldSetup = this._currentSetup;
+		this._currentSetup = { ...oldSetup, ram };
+	}
+
+	currentSetup() {
+		return this._currentSetup;
 	}
 }
 
-export {Parser};
+export { Parser };
